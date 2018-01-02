df11a8b6203e7e2b37ec1c703e8ebd90c29839bb
edeep <35022196+edeep@users.noreply.github.com>
2018-01-02 13:33:07 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -8,14 +8,32 @@ class App extends Component {
     person: [{ name: "pradeep", power: "12" }, { name: "esha", power: "100" }]
   };
 
+  showPower = (newName)=>{
+   this.setState({
+     person: [{ name: newName, power: "12" }, { name: "eshaUpdated", power: "100" }]
+   });
+
+  };
+
+  changeName = (event) => {
+    this.setState({
+      person: [{ name: event.target.value, power: "12" }, { name: "eshaUpdated", power: "100" }]
+    });
+
+  };
+
   render() {
     return (
       <div className="App">
         <p >
           React app Begins!!
         </p>
-        <Person name={this.state.person[0].name}> Eat well </Person>
-        <Person name={this.state.person[1].name}> play well </Person>
+         <button onClick={()=>this.showPower('Batman')}> Show Power</button>
+        <Person changename={this.changeName} name={this.state.person[0].name}> Eat well </Person>
+        <Person click={this.showPower.bind(this, 'Superman')}  name={this.state.person[1].name}> 
+        play well </Person>
+        <Person 
+        name={this.state.person[0].name}> Sleep well </Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 var person = (props)=>{
   return (
-    <div>
-    <p> I am the person - {props.name} </p>
+    <div className="Person">
+    <p onClick={props.click}> I am the person - {props.name} </p>
     <p> What I say is - {props.children} </p>
+    <input type="text" onChange={props.changename}/>
     </div>
   );
 }
